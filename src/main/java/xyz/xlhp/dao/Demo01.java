8825918e29d3b4bdeee66d99f0b4f49b86f64aa7
丁兆顺 <3128439397@qq.com>
2019-07-08 21:31:01 +0800

    git from idea ; time : 2019-07-08 21:29

---
+++ src/main/java/xyz/xlhp/dao/Demo01.java
@@ -0,0 +1,12 @@
+package xyz.xlhp.dao;
+
+/**
+ * @author 丁兆顺
+ * @date 2019/7/8 21:15
+ * @desciption
+ */
+public class Demo01 {
+        public static void main(String[] args){
+            System.out.printf("git初始化并提交-01");
+        }
+}
